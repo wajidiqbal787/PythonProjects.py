b6865249198ef7b3f3268f8fc25d6209976a0124
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-10 11:57:20 +0500

    Project-No-01

---
+++ Project-No-01.py
@@ -0,0 +1,8 @@
+print("\n--------------- Greeting ---------------")
+name = "Wajid Iqbal"
+age = "3467"
+role = "Python Developer"
+info = "My Name is " + name + " I am " + age + " years Old and My Role is " + role + "."
+infoo = f"My Name is {name} I am {age} years Old \nand My Role is {role}."
+print(info)
+print(infoo)
