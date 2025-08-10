cf601f0a002055cd5ed2f32776f50aac1e01a86b
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-10 12:06:14 +0500

    Project-No-03

---
+++ Project-No-03.py
@@ -0,0 +1,11 @@
+#Welcome To Message Generator
+#Step No 01: Ask For User details
+name = input("What is your name? \n")
+hobby = "Photography"
+
+#Step No 02: Generate A Peronalized Welcome Message
+print("\n ----- Welcome To Message Generator -----")
+print(f"Hello, {name}!.😄")
+print(f"Welcome To The World Of Python Programming. 😲")
+print(f"It is great to know that you love {hobby}. 🥰")
+print(f"Get Ready To Build Something Amazing Today. 👻")
