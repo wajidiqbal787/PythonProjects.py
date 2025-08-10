379517d155f3d2555f07403e1b764cb9d85bf6da
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-10 12:03:21 +0500

    Project-No-03

---
+++ Project-No-03.py
@@ -0,0 +1,4 @@
+print("\n------------ Welcome To Group Name Generator ------------")
+color = input("What is your favourite color?\n")
+animal = input("What is your favourite animal?\n")
+print("Your group name could be " + color + " " + animal + "s")
