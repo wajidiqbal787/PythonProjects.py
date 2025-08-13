8a6063187af63706f64fcea9cbe609bfa44d295d
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:40:29 +0500

    Project-No-01

--- Project-No-01.py
+++ Project-No-01.py
@@ -1,3 +1,8 @@
+# QUESTION NO 01:
+# Write a Python program that prints a greeting header and displays a person's name, age, and role in two different ways:
+# First, by using string concatenation.
+# Second, by using an f-string with a newline before the role.
+
 print("\n--------------- Greeting ---------------")
 name = "Wajid Iqbal"
 age = "3467"
