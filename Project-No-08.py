a5e6d6d8e9cdfdb0bc90b7b9326cd50151b77d53
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:36:42 +0500

    Project-No-08

--- Project-No-08.py
+++ Project-No-08.py
@@ -1,4 +1,4 @@
-# Qno 01: Write a program which prompts the user for a celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature. Formula: (C x 9/5) + 32 = F
+# Question No 08: Write a program which prompts the user for a celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature. Formula: (C x 9/5) + 32 = F
 
 print("\n------------ Temperature Converter ------------ ")
 celsius = int(input("Enter Temperature In Celsius: \n"))
