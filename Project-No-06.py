ec7cb5923d636aad5440551c64d42a840f2343a6
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:51:05 +0500

    Project-No-06

--- Project-No-06.py
+++ Project-No-06.py
@@ -1,3 +1,10 @@
+# QUESTION NO 06:
+# Write a Python program that:
+# Displays a header for a Gross Pay Project.
+# Asks the user to enter the number of hours worked and the hourly rate.
+# Converts both inputs to floating-point numbers.
+# Calculates and displays the gross pay by multiplying hours with rate.
+
 print("GROSS PAY PROJECT")
 hour = input("Enter Hours: \n")
 rate = input("Enter Rate: \n")
