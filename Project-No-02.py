e0805ce21ec9d052be732284e1107d68ad32dd12
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:42:29 +0500

    Project-No-02

--- Project-No-02.py
+++ Project-No-02.py
@@ -1,3 +1,10 @@
+# QUESTION NO 02:
+# Write a Python program that:
+# Prints a header line with the text.
+# Asks the user to enter their name.
+# Greets the user by printing "Hello! <name>".
+# Prints "Welcome To Python Programming" on the next line.
+
 print("\n------------ Greeting Generator ------------")
 name = input("Enter Your Name?\n")
 print("Hello! " + name)
