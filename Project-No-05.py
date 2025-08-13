07e66d3b5b82622baaacf54f1fcc34a63893c1da
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:49:38 +0500

    Project-No-05

--- Project-No-05.py
+++ Project-No-05.py
@@ -1,3 +1,9 @@
+# QUESTION NO 05:
+# Write a Python program that:
+# Prints a welcome header for a Group Name Generator.
+# Asks the user for their favourite color and favourite animal.
+# Combines these inputs to suggest a possible group name.
+
 print("\n------------ Welcome To Group Name Generator ------------")
 color = input("What is your favourite color?\n")
 animal = input("What is your favourite animal?\n")
