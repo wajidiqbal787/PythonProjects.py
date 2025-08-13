b00b68a8f9098a7deb74cdd03f7c05276a383451
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 13:02:49 +0500

    Project-No-11

---
+++ Project-No-11.py
@@ -0,0 +1,15 @@
+# QUESTION NO 11:
+# Write a Python program that:
+# Displays a header for Area of a Circle.
+# Asks the user to enter the radius of the circle.
+# Calculates the area of the circle using the formula:
+# Area = 3.14159 × radius × radius
+# Displays the calculated area in a clear format.
+
+
+
+print("\n---------- Area Of A Circle ----------")
+radius = float(input("Enter Your Radius? \n"))
+formula = 3.14159 * radius * radius
+
+print(f"Your Circle Value is {formula}")
