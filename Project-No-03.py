fa76406ca33dd7ada146ec76b3abde111645c76f
wajidiqbal787 <wajidiqbalhoun@gmail.com>
2025-08-13 12:44:41 +0500

    Project-No-03

--- Project-No-03.py
+++ Project-No-03.py
@@ -1,3 +1,14 @@
+# QUESTION NO 03:
+# Write a Python program that:
+# Asks the user for their name.
+# Stores "Photography" in a variable named hobby.
+# Prints a header line with the text.
+# Displays a personalized welcome message using an f-string, including:
+# "Hello, <name>!.😄"
+# "Welcome To The World Of Python Programming. 😲"
+# "It is great to know that you love Photography. 🥰"
+# "Get Ready To Build Something Amazing Today. 👻"
+
 #Welcome To Message Generator
 #Step No 01: Ask For User details
 name = input("What is your name? \n")
